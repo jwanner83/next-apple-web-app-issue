1170de6a2d325b24309343921e8705cb514ecd85
Jonas Wanner <jonas.wanner@clavisit.com>
2025-01-04 22:26:17 +0100

    init

---
+++ working-with-workaround/app/layout.tsx
@@ -0,0 +1,172 @@
+import {Metadata} from 'next'
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: 'Working with workaround',
+    appleWebApp: true,
+    manifest: '/manifest.json',
+    metadataBase: new URL('https://example.com'),
+    icons: [
+      {
+        rel: 'apple-touch-icon',
+        url: 'icon.png'
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        url: 'apple-splash-2048-2732.jpg',
+        media:
+          '(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        url: 'apple-splash-2732-2048.jpg',
+        media:
+          '(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)'
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        url: 'apple-splash-1668-2388.jpg',
+        media:
+          '(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        url: 'apple-splash-2388-1668.jpg',
+        media:
+          '(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)'
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        url: 'apple-splash-1536-2048.jpg',
+        media:
+          '(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        url: 'apple-splash-2048-1536.jpg',
+        media:
+          '(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)'
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        url: 'apple-splash-1488-2266.jpg',
+        media:
+          '(device-width: 744px) and (device-height: 1133px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        url: 'apple-splash-2266-1488.jpg',
+        media:
+          '(device-width: 744px) and (device-height: 1133px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)'
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        url: 'apple-splash-1640-2360.jpg',
+        media:
+          '(device-width: 820px) and (device-height: 1180px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        url: 'apple-splash-2360-1640.jpg',
+        media:
+          '(device-width: 820px) and (device-height: 1180px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)'
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        url: 'apple-splash-1668-2224.jpg',
+        media:
+          '(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        url: 'apple-splash-2224-1668.jpg',
+        media:
+          '(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)'
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        url: 'apple-splash-1620-2160.jpg',
+        media:
+          '(device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        url: 'apple-splash-2160-1620.jpg',
+        media:
+          '(device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)'
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        url: 'apple-splash-1290-2796.jpg',
+        media:
+          '(device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)'
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        url: 'apple-splash-1179-2556.jpg',
+        media:
+          '(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)'
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        url: 'apple-splash-1284-2778.jpg',
+        media:
+          '(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)'
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        url: 'apple-splash-1170-2532.jpg',
+        media:
+          '(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)'
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        url: 'apple-splash-1125-2436.jpg',
+        media:
+          '(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)'
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        url: 'apple-splash-1242-2688.jpg',
+        media:
+          '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)'
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        url: 'apple-splash-828-1792.jpg',
+        media:
+          '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        url: 'apple-splash-1242-2208.jpg',
+        media:
+          '(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)'
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        url: 'apple-splash-750-1334.jpg',
+        media:
+          '(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        url: 'apple-splash-640-1136.jpg',
+        media:
+          '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
+      }
+    ]
+  }
+}
+
+export default function RootLayout({children}) {
+  return (
+    <html>
+    <head>
+      {/* WORKAROUND */}
+      <meta name="apple-mobile-web-app-capable" content="yes"/>
+    </head>
+    <body>{children}</body>
+    </html>
+  )
+}
